14d8a9d8607fceb9b54ceaba00f8496bbcbade1b
✪ 1tap <thatboyfrankie@outlook.com>
2017-09-21 17:54:15 -0700

    Files
    Files

---
+++ Framework/Cheat.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "SDK.h"
+#include "Settings\Vars.h"
+#include "Settings\Config.h"
+#include "SDK\Draw.h"
+#include "Hooks.h"
+#include "Enhancements.h"
+#include "SDK\Input.h"
+
+
+#define CSGO_VERSION charenc("1.35.7.9")
